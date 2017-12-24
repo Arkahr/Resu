bdfb54ecf79ba77816e365ec0572ef19661c2946
User5981 <13667502+User5981@users.noreply.github.com>
2017-12-24 10:32:18 +0100

    Added Seasonal + improved

--- HotEnablerDisablerPlugin.cs
+++ HotEnablerDisablerPlugin.cs
@@ -1,5 +1,5 @@
 // https://github.com/User5981/Resu
-// Hot Enabler/Disabler Plugin for TurboHUD Version 14/12/2017 15:03
+// Hot Enabler/Disabler Plugin for TurboHUD Version 24/12/2017 10:24
 
 using System;
 using System.Collections.Generic;
@@ -12,13 +12,18 @@ namespace Turbo.Plugins.Resu
     public class HotEnablerDisablerPlugin : BasePlugin 
 	{
 
-       public Dictionary<string,string> DisableThis { get; set; }
-				
+       public Dictionary<string,string> DisableThatGameMode { get; set; }
+	   public Dictionary<string,string> DisableThatGameType { get; set; }
+       public Dictionary<string,string> DisableTheseHeroClasses { get; set; }
+       public Dictionary<string,string> DisableTheseHeroNames { get; set; }	   
 		
 		public HotEnablerDisablerPlugin()
 		{
             Enabled = true;
-			DisableThis = new Dictionary<string,string>();
+			DisableThatGameMode = new Dictionary<string,string>();
+			DisableThatGameType = new Dictionary<string,string>();
+			DisableTheseHeroClasses = new Dictionary<string,string>();
+			DisableTheseHeroNames = new Dictionary<string,string>();
 		}
 
         public override void Load(IController hud)
@@ -32,20 +37,48 @@ namespace Turbo.Plugins.Resu
 		public bool CanIRun(IPlayer me, string ThisPlugin)
              {
 		      bool Hardcore = me.HeroIsHardcore;
+			  bool Seasonal = me.Hero.Seasonal;
 		      string Heroclass = me.HeroClassDefinition.HeroClass.ToString();
-		      var Exclude = "";
+			  var ExcludeGameMode = "";
+			  var ExcludeGameType = "";
+			  var ExcludeHeroClasses = "";
+			  var ExcludeHeroNames = "";
+			  
+			  
 		
-		      if (DisableThis.TryGetValue(ThisPlugin, out Exclude)) 
+		      if (DisableThatGameMode.TryGetValue(ThisPlugin, out ExcludeGameMode)) 
                  {
-        		  if (Hardcore && Exclude == "Hardcore") return false;
-		          else if (!Hardcore && Exclude == "Softcore") return false;
-				  else if (Exclude.Contains(Heroclass)) return false;
-				  else if (Exclude.Contains(me.HeroName)) return false;
-                  else return true;		  
-			     }
-		      else return true;
+        		  if (Hardcore && ExcludeGameMode == "Hardcore") return false;
+		          else if (!Hardcore && ExcludeGameMode == "Softcore") return false;
+				  else goto NoGameMode;
+				 }
+			  else goto NoGameMode;
+			  
+			 NoGameMode:
+			 if (DisableThatGameType.TryGetValue(ThisPlugin, out ExcludeGameType))
+				{
+				 if (Seasonal && ExcludeGameType == "Seasonal") return false;
+		         else if (!Seasonal && ExcludeGameType == "NonSeasonal") return false;  
+				 else goto NoGameType;
+				} 
+			 else goto NoGameType;	
+				
+              NoGameType: 
+              if (DisableTheseHeroClasses.TryGetValue(ThisPlugin, out ExcludeHeroClasses))
+				 {
+                  if (ExcludeHeroClasses.Contains(Heroclass)) return false;
+	              else goto NoHeroClass;
+				 }
+			  else goto NoHeroClass; 
+				 
+			  NoHeroClass:						 
+			  if (DisableTheseHeroNames.TryGetValue(ThisPlugin, out ExcludeHeroNames))
+				 {
+				  if (ExcludeHeroNames.Contains(me.HeroName)) return false;  
+				  else return true;  
+				 }
+			  else return true; 			  
 			  }
-	        
     }
 
 }
