3be0dcfa411c6187a220712cb0a78919df595bc4
User5981 <13667502+User5981@users.noreply.github.com>
2018-07-07 22:02:11 +0200

    Optimization

--- GroupGRLevelAdviserPlugin.cs
+++ GroupGRLevelAdviserPlugin.cs
@@ -1,5 +1,5 @@
 // https://github.com/User5981/Resu
-// Group GR Level Adviser Plugin for TurboHUD version 07/07/2018 07:47
+// Group GR Level Adviser Plugin for TurboHUD version 07/07/2018 21:08
 using Turbo.Plugins.Default;
 using System;
 using System.Collections.Generic;
@@ -12,10 +12,6 @@ namespace Turbo.Plugins.Resu
     {
         public TopLabelDecorator GRLevelDecorator { get; set; }
         public string GRLevelText{ get; set; }
-        public int MaxGRPlayer0 { get; set; }
-        public int MaxGRPlayer1 { get; set; }
-        public int MaxGRPlayer2 { get; set; }
-        public int MaxGRPlayer3 { get; set; }
         
         public GroupGRLevelAdviserPlugin()
         {
@@ -26,13 +22,7 @@ namespace Turbo.Plugins.Resu
         public override void Load(IController hud)
         {
          base.Load(hud);
-         
-         MaxGRPlayer0 = 0;
-         MaxGRPlayer1 = 0;
-         MaxGRPlayer2 = 0;
-         MaxGRPlayer3 = 0;
          GRLevelText = "";
-         
          
          GRLevelDecorator = new TopLabelDecorator(Hud)
           {
@@ -44,41 +34,20 @@ namespace Turbo.Plugins.Resu
         
         
          public void PaintWorld(WorldLayer layer)
-        {
-         var players = Hud.Game.Players;
-         foreach (var player in players)
          {
-          if (player.PortraitIndex == 0) MaxGRPlayer0 = player.HighestSoloRiftLevel;
-          if (player.PortraitIndex == 1) MaxGRPlayer1 = player.HighestSoloRiftLevel;
-          if (player.PortraitIndex == 2) MaxGRPlayer2 = player.HighestSoloRiftLevel;
-          if (player.PortraitIndex == 3) MaxGRPlayer3 = player.HighestSoloRiftLevel;
-         }
-         
-         if (Hud.Game.NumberOfPlayersInGame == 1) return;
-         if (Hud.Game.NumberOfPlayersInGame == 2)
-          {
-           int GRAvereage = Convert.ToInt32(Convert.ToDouble(((MaxGRPlayer0 + MaxGRPlayer1)/2) + 2));
-           GRLevelText = "" + GRAvereage;
-          }
-         if (Hud.Game.NumberOfPlayersInGame == 3) 
-          {
-           int GRAvereage = Convert.ToInt32(Convert.ToDouble(((MaxGRPlayer0 + MaxGRPlayer1 + MaxGRPlayer2)/3) + 4));
-           GRLevelText = "" + GRAvereage;
-          }
-         if (Hud.Game.NumberOfPlayersInGame == 4)
-          {
-           int GRAvereage = Convert.ToInt32(Convert.ToDouble(((MaxGRPlayer0 + MaxGRPlayer1 + MaxGRPlayer2 + MaxGRPlayer3)/4) + 6));
-           GRLevelText = "" + GRAvereage;
-          }
-          
-         bool GRPanel = Hud.Render.GetUiElement("Root.NormalLayer.rift_dialog_mainPage").Visible;
-         
-         if (GRPanel)
-          {
-           var uiRect = Hud.Render.GetUiElement("Root.NormalLayer.rift_dialog_mainPage").Rectangle;
-           GRLevelDecorator.Paint(uiRect.Left, uiRect.Top, uiRect.Width, uiRect.Height, HorizontalAlign.Right);
-          }
-
+          if (Hud.Game.NumberOfPlayersInGame > 1 && Hud.Render.GetUiElement("Root.NormalLayer.rift_dialog_mainPage").Visible)
+           {
+            int maxGRlevel = 0;
+            foreach (var player in Hud.Game.Players)
+                  {
+                   maxGRlevel +=  player.HighestSoloRiftLevel;
+                  }
+           
+            int GRAverage = Convert.ToInt32(Convert.ToDouble(maxGRlevel / Hud.Game.NumberOfPlayersInGame + (2 * (Hud.Game.NumberOfPlayersInGame - 1))));
+            GRLevelText = GRAverage.ToString();
+            var uiRect = Hud.Render.GetUiElement("Root.NormalLayer.rift_dialog_mainPage").Rectangle;
+            GRLevelDecorator.Paint(uiRect.Left, uiRect.Top, uiRect.Width, uiRect.Height, HorizontalAlign.Right);
+           }
         }
     }
 
